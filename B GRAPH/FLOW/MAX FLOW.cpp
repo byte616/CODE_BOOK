e26753c51c7a7ca5677f379b4ce0dd6bc67da6af
byte616 <p500297@gmail.com>
2023-08-21 09:10:34 +0800

    update max_flow(dinic)

--- B GRAPH/FLOW/MAX FLOW.cpp
+++ B GRAPH/FLOW/MAX FLOW.cpp
@@ -1,21 +1,40 @@
-ll n,m,a,b,c,s,n;
-bool vis[MAX_N];
 struct edge{
-    int to;ll cap;int rev;
-    edge(){};
-    edge(int _a,ll _b,int _c):to(_a),cap(_b),rev(_c){}
+    int to; ll cap; int rev;
+    edge(){}
+    edge(int _to, ll _cap, int _rev): to(_to), cap(_cap), rev(_rev){}
+};
+vector<edge> v[MAX_N];
+ll n, m, a, b ,c, s, t;
+int level[MAX_N], iter[MAX_N];
+void add_edge(int a, int b, ll c){
+    v[a].push_back(edge(b, c, v[b].size()));
+    v[b].push_back(edge(a, 0, v[a].size() - 1));
 }
-vector<edge>v[MAX_N];
-ll dfs(int now,ll flow){// s -> t
-    vis[now]=1;
-    if(now==t)return flow;
-    for(int i=0;i<v[now].size();i++){
-        edge &e=v[now][i];
-        if(e.cap>0&&!vis[e.to]){
-            ll ret=dfs(e.to,min(flow,e.cap));
-            if(ret>0){
-                e.cap-=ret;
-                v[e.to][e.rev].cap+=ret;
+void BFS(){
+    memset(level, -1, sizeof(level));
+    level[s] = 0;
+    queue<int> q;
+    q.push(s);
+    while(!q.empty()){
+        int cur = q.front();
+        q.pop();
+        for(auto e : v[cur]){
+            if(e.cap > 0 && level[e.to] == -1){
+                level[e.to] = level[cur] + 1;
+                q.push(e.to);
+            }
+        }
+    }
+}
+ll dfs(int now, ll flow){ // s -> t
+    if(now == t) return flow;
+    for(int &i = iter[now]; i < v[now].size(); i++){
+        edge &e = v[now][i];
+        if(e.cap > 0 && level[e.to] == level[now] + 1){
+            ll ret = dfs(e.to, min(flow, e.cap));
+            if(ret > 0){
+                e.cap -=ret;
+                v[e.to][e.rev].cap += ret;
                 return ret;
             }
         }
@@ -23,19 +42,19 @@ ll dfs(int now,ll flow){// s -> t
     return 0;
 }
 ll max_flow(){
-    ll ret=0,tmp=0;
-    while((tmp=dfs(1,INF))>0){//INF->1e18
-        ret+=tmp;
-        memset(vis,0,sizeof(vis));
+    ll ret = 0;
+    while(1){
+        BFS();
+        if(level[t] == -1) break;
+        memset(iter, 0, sizeof(iter));
+        ll tmp;
+        while((tmp = dfs(s, INF)) > 0) ret += tmp;
     }
     return ret;
-}
-void add_edge(int a,int b,int c){
-    v[a].push_back(edge(b,c,v[b].size()));
-    v[b].push_back(edge(a,0,v[a].size()-1));
 }
 main(){
     s=1,t=n;
     while(m--)add_edge(a,b,c);
     cout<<max_flow();
 }
+
