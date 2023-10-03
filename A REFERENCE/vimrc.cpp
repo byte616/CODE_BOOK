7a380163a861f0fc8ed1084d5f640e4ba805aa07
byte616 <p500297@gmail.com>
2023-10-03 11:31:33 +0800

    fix vimrc

--- A REFERENCE/vimrc.cpp
+++ A REFERENCE/vimrc.cpp
@@ -2,31 +2,19 @@
 // i o a enter -> insert
 // <ESC> -> normal -> wq: save and quit
 // v -> virtual -> y: crtl+c  p: crtl+v
+
+// must
 set nu
 set cursorline
-set ruler
-set linebreak
-set showcmd
-set scrolloff=5
-set hlsearch
-set ignorecase
-set insearch
-
 syntax on
-colorscheme default
-
+set autoindent
+set smartindent
 set tabstop=4
+set shiftwidth=4
 set expandtab
-set shiftwidth=2
 set ai
-map <Tab> >>
-map <S-Tab> <<
 set mouse=a
-set noswapfile
-
-imap <Enter> o
-inoremap ( ()<ESC>i
-inoremap [ []<ESC>i
-inoremap ' ''<ESC>i
-inoremap " ""<ESC>i
+noremap <Enter> i
 inoremap {<CR> {<CR>}<Esc>O
+
+
